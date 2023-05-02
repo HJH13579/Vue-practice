deb2ace7cc98fa59cdbb44c89521d35335762525
heojoohyeok <heojoohyeok@naver.com>
2023-05-02 17:53:37 +0900

    add day3

---
+++ day3/vue-cli/src/components/MyComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="border">
+    <h1>This is Component</h1>
+    <MyChild /> 
+    <MyChild /> 
+    <MyChild /> 
+    <MyChild /> 
+  </div>
+  <!-- <div></div> : error, 루트는 하나여야 한다. -->
+</template>
+
+<script>
+import MyChild from '@/components/MyChild.vue'
+
+export default {
+  name: 'MyComponent',
+  components : {
+    MyChild,
+  }
+}
+</script>
+
+<style>
+.border {
+  border: solid;
+}
+</style>
