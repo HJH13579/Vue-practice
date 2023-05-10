570a370f14ffc0b18dbb89b4686f372042ad3321
heojoohyeok <heojoohyeok@naver.com>
2023-05-10 17:52:27 +0900

    add day8

---
+++ day8/boardlist/vue-pjt/src/views/DetailView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>Detail</h1>
+    <h1>제목 : {{ article?.title }}</h1>
+    <h1>내용 : {{ article?.content }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailView',
+  data() {
+    return {
+      article : {}
+    }
+  },
+  methods:{
+    fetchArticle(articleId) {
+      const articles = this.$store.state.articles
+      console.log(articles)
+      const article = articles.filter((article) => {
+        return article.id == articleId
+      })[0]
+      this.article = article
+    }
+  },
+  created() {
+    const articleId = this.$route.params.id
+    // axios.get(하나의 article 가져와줘)
+    // this.$store.dispatch('fetchArticle', articleId)
+    // 창고에서 하나의 article을 가져온다.
+
+    // axios 대체
+    this.fetchArticle(articleId)
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ day8/boardlist/vue-pjt/src/views/IndexView.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>Index</h1>
+    <ArticleItem 
+    v-for="article in articles"
+    :key="article.id"
+
+    :article="article"
+    />
+  </div>
+</template>
+
+<script>
+import ArticleItem from '../components/ArticleItem.vue'
+
+export default {
+  name: 'IndexView',
+  computed: {
+    articles() {
+      return this.$store.state.articles
+    }
+  },
+  created() {
+    // 장고에게 axios 요청을 보내서 state에 있는 articles를 채워줘야합니다.
+    // axios 요청을 통해서 받았다고 가정 => actions로 들어감
+    this.$store.dispatch('fetchArticles')
+
+    // articles = [
+    //   {
+    //     id: 1,
+    //     title: '제목1',
+    //     content: '내용1',
+    //   },
+    //   {
+    //     id: 2,
+    //     title: '제목2',
+    //     content: '내용2',
+    //   }
+    // ]
+  }
+}
+</script>
+
+<style>
+
+</style>
